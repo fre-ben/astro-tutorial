c92cbe92b4299cd886a542781965f07bbf50c2fc
frederik.schocke <frederik.schocke@aoe.com>
2023-06-11 12:36:12 +0200

    feat: add two pages

---
+++ .prettierrc.cjs
@@ -0,0 +1,13 @@
+// prettierrc.cjs
+module.exports = {
+  pluginSearchDirs: [__dirname],
+  plugins: [require.resolve("prettier-plugin-astro")],
+  overrides: [
+    {
+      files: "*.astro",
+      options: {
+        parser: "astro",
+      },
+    },
+  ],
+};
